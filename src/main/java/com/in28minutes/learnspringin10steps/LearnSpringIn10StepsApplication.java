ce3f4cf0bd263f18c5aae660f8117fb15aa6d904
RahulGupta <rahulrgd21@gmail.com>
2024-02-19 21:33:37 +0530

    Step 03 - Iteration 1 - Building Tightly Coupled GameRunner and MarioGame

--- src/main/java/com/in28minutes/learnspringin10steps/LearnSpringIn10StepsApplication.java
+++ src/main/java/com/in28minutes/learnspringin10steps/LearnSpringIn10StepsApplication.java
@@ -3,11 +3,17 @@ package com.in28minutes.learnspringin10steps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.learnspringin10steps.game.GameRunner;
+import com.in28minutes.learnspringin10steps.game.MarioGame;
+
 @SpringBootApplication
 public class LearnSpringIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringIn10StepsApplication.class, args);
+		// SpringApplication.run(LearnSpringIn10StepsApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
 	}
 
 }
---
+++ src/main/java/com/in28minutes/learnspringin10steps/game/MarioGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringin10steps.game;
+
+public class MarioGame {
+
+    public void up(){
+        System.out.println("Mario look Up!");
+    }
+
+    public void down(){
+        System.out.println("Mario look Down!");
+    }
+
+    public void left(){
+        System.out.println("Mario look Left!");
+    }
+
+    public void right(){
+        System.out.println("Mario look Right!");
+    }
+}
