ae065f0c2e105065a4d0a555081d4b712fe8c3cb
RahulGupta <rahulrgd21@gmail.com>
2024-02-19 21:46:20 +0530

    Created PacManGame as an exercise!

--- src/main/java/com/in28minutes/learnspringin10steps/LearnSpringIn10StepsApplication.java
+++ src/main/java/com/in28minutes/learnspringin10steps/LearnSpringIn10StepsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.in28minutes.learnspringin10steps.game.GameRunner;
 import com.in28minutes.learnspringin10steps.game.MarioGame;
+import com.in28minutes.learnspringin10steps.game.PacManGame;
 import com.in28minutes.learnspringin10steps.game.SuperContraGame;
 
 @SpringBootApplication
@@ -12,7 +13,8 @@ public class LearnSpringIn10StepsApplication {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(LearnSpringIn10StepsApplication.class, args);
-		MarioGame game = new MarioGame();
+		// MarioGame game = new MarioGame();
+		PacManGame game = new PacManGame();
 		// SuperContraGame game = new SuperContraGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
---
+++ src/main/java/com/in28minutes/learnspringin10steps/game/PacManGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringin10steps.game;
+
+public class PacManGame implements GamingConsole{
+
+    public void up(){
+        System.out.println("PacMan look Up!");
+    }
+
+    public void down(){
+        System.out.println("PacMan look Down!");
+    }
+
+    public void left(){
+        System.out.println("PacMan look Left!");
+    }
+
+    public void right(){
+        System.out.println("PacMan look Right!");
+    }
+}
