15a275666e1d16e3e5cba4bc3c86fa47b5642411
RahulGupta <rahulrgd21@gmail.com>
2024-02-19 21:17:09 +0530

    Step 02 - Setting up New Spring Project with Spring Initializr

---
+++ src/main/java/com/in28minutes/learnspringin10steps/LearnSpringIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnspringin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringIn10StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/learnspringin10steps/LearnSpringIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnspringin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
