c89547ba04d5a19de9de37cc3203b32449e0822c
RahulGupta <rahulrgd21@gmail.com>
2024-02-20 01:21:29 +0530

    Step 13 - Exploring Spring Framework Dependency Injection Types

--- src/main/java/com/in28minutes/learnspringin10steps/enterprise/example/business/BusinessService.java
+++ src/main/java/com/in28minutes/learnspringin10steps/enterprise/example/business/BusinessService.java
@@ -1,23 +1,32 @@
 package com.in28minutes.learnspringin10steps.enterprise.example.business;
 
+import com.in28minutes.learnspringin10steps.enterprise.example.data.DataService;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.in28minutes.learnspringin10steps.enterprise.example.data.DataService;
 
 @Component
 public class BusinessService {
 
+  // Any one of the Injection would have worked, either Field, Constructor, Setter 
+  // Recommended method is - Constructor Injection
+  // No need to use all three methods to inject dependencies - Although it won't give any error
+
+  // Field Injection of dependencies
   @Autowired
   private DataService dataService;
-  
 
+  // Construtor Injection of Dependencies
+  @Autowired
   public BusinessService(DataService dataService) {
     this.dataService = dataService;
   }
 
+  // Setter Injection of Dependencies
+  @Autowired
+  public void setDataService(DataService dataService) {
+    this.dataService = dataService;
+  }
 
   public long calculateSum() {
     List<Integer> data = dataService.getData();
